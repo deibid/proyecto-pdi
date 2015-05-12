b0b9ae0081dab22710fb7ee8308ed47ec6fba26c
Pablo <pablotortoriello@gmail.com>
2015-05-12 14:48:26 -0500

    cambio nombres imagenes

--- main.m
+++ main.m
@@ -9,11 +9,7 @@ clc
 close all
 clear
 
-
-
-
-
-Ic = imread('Imagenes/IMG_9730.jpg');
+Ic = imread('nina 1.jpg');
 I = rgb2gray(Ic);
 
 bloques = blockproc(I,[70,70],@mediana);
@@ -31,29 +27,22 @@ Ic(:,:,2) = Ic(:,:,2)-bloques;
 Ic(:,:,3) = Ic(:,:,3)-bloques;
 imshow(Ic);
 
-
-global count
-global x
-[count, x] = imhist(Ic(:,:,1));
-y = find(count == max(max(count)))
-[count, x] = imhist(Ic(:,:,2));
-y2 = find(count == max(max(count)))
-[count, x] = imhist(Ic(:,:,3));
-y3 = find(count == max(max(count)))
+[count] = imhist(Ic(:,:,1));
+yR = find(count == max(max(count)));
+[count] = imhist(Ic(:,:,2));
+yG = find(count == max(max(count)));
+[count] = imhist(Ic(:,:,3));
+yB = find(count == max(max(count)));
 
 
-inf = y - 30;
-sup = y + 30;
-inf2 = y2 - 30;
-sup2 = y2 + 30;
-inf3 = y3 - 30;
-sup3 = y3 + 30;
+infR = yR - 30;
+supR = yR + 30;
+infG = yG - 30;
+supG = yG + 30;
+infB = yB - 30;
+supB = yB + 30;
 
-ImBin = Ic(:,:,1)>sup  | Ic(:,:,1)<inf;
-ImBin2= Ic(:,:,2)>sup2 | Ic(:,:,2)<inf2;
-ImBin3= Ic(:,:,3)>sup3 | Ic(:,:,3)<inf3;
-
-im = ImBin == 1 | ImBin2 == 1 | ImBin3 == 1;
+im = (Ic(:,:,1)>supR  | Ic(:,:,1)<infR) | (Ic(:,:,2)>supG | Ic(:,:,2)<infG) | (Ic(:,:,3)>supB | Ic(:,:,3)<infB);
 
 figure, imshow(im);
 im = imclearborder(im);
@@ -64,16 +53,6 @@ F = L == 12;
 figure, imshow(F);
 
 datos = regionprops(F, 'Area','ConvexArea','Centroid','MajorAxisLength','MinorAxisLength');
-disp('area');
-datos.Area
-disp('convexarea');
-datos.ConvexArea
-disp('centroid');
-datos.Centroid
-disp('maj axis len');
-datos.MajorAxisLength
-disp('min axis len');
-datos.MinorAxisLength
 
 end
 
