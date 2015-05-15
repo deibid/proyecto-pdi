381f55bd82043061e57aa50faf1af5ceedb9b7cd
deibid <davidazar3@gmail.com>
2015-05-14 20:33:20 -0500

    promedia 6 imagenes de un objeto, y creoa vector. Generar loop externo y terminar clasificador

--- main.m
+++ main.m
@@ -42,17 +42,25 @@ parametros = {'Area'
 jpg = '.jpg';
 
 
+global datosImagen;
+datosImagen = zeros(14,1,6);
+    %14 caracteristicas, 1 tipo de figura, 6 imagenes
+
+
+
 k = 1;
 
-%for i = 1:16
-a = figuras{10};
-for j = 1:1
-    path = [a  num2str(j) jpg];
+%iteracion sobre las fotos del mismo tipo
+a = figuras{5};
+for j = 1:6
+    
+    path = [a  num2str(j) jpg]
     k = k+1;
-    %end
-    %end
+    
     
     Ic = imread(path);
+    if(j==6)figure,imshow(Ic) 
+    end
     I = rgb2gray(Ic);
     
     bloques = blockproc(I,[70,70],@mediana);
@@ -69,6 +77,7 @@ for j = 1:1
     Ic(:,:,2) = Ic(:,:,2)-bloques;
     Ic(:,:,3) = Ic(:,:,3)-bloques;
     %figure, imshow(Ic);
+   
     
     [count] = imhist(Ic(:,:,1));
     yR = find(count == max(max(count)));
@@ -95,78 +104,66 @@ for j = 1:1
     imBin = imfill(imBin,'holes');
     %figure, imshow(imBin);
     
-    
-    [L, numObj] = bwlabel(imBin);
-    global datosImagen;
-    %EN CASO DE TERMINAR USANDO ESTRUCTURAS..%
-    %datosImagen = struct('Area',{0},'MajorAxisLength',{0},'MinorAxisLength',{0},'Eccentricity',{0},'EquivDiameter',{0},'Extent',{0},'Perimeter',{0},'AxisRatio',{0});
-    
-    datosImagen = zeros(14,1,3);
-    
-    %CAPA 1 - PROMEDIOS
-    %CAPA 2 - MINIMOS
-    %CAPA 3 - MAXIMOS
-    
-    for l=1 :numObj
-        
-        %datos = regionprops(L == l, 'Area', 'MajorAxisLength','MinorAxisLength','Eccentricity','EquivDiameter','Extent','Perimeter');
-        datos = regionprops(L == l, parametros);
-        
-        %%min y max de cada campo
-        
-        
-        datosImagen(1,1,1) = datosImagen(1,1,1) + datos.Area;
-        datosImagen(2,1,1) = datosImagen(2,1,1) + datos.MajorAxisLength;
-        datosImagen(3,1,1) = datosImagen(3,1,1) + datos.MinorAxisLength;
-        datosImagen(4,1,1) = datosImagen(4,1,1) + datos.Eccentricity;
-        datosImagen(5,1,1) = datosImagen(5,1,1) + datos.EquivDiameter;
-        datosImagen(6,1,1) = datosImagen(6,1,1) + datos.Extent;
-        datosImagen(7,1,1) = datosImagen(7,1,1) + datos.Perimeter;
-        datosImagen(8,1,1) = datosImagen(8,1,1) + datos.ConvexArea;
-        datosImagen(9,1,1) = datosImagen(9,1,1) + datos.Solidity;
-        %HASTA AQUI SON PARAMETROS OBTENIDOS DIRECTAMENTE%
-        %ESTOS SON PARAMETROS COMBINADOS%
-        datosImagen(10,1,1) = datosImagen(10,1,1) + (datos.MinorAxisLength/datos.MajorAxisLength);%AxisRatio
-        datosImagen(11,1,1) = datosImagen(11,1,1) + (datos.MajorAxisLength*datos.MinorAxisLength);%AreaRect
-        datosImagen(12,1,1) = datosImagen(12,1,1) + ((datos.Area * 4 * pi)/(datos.Perimeter^2));%FormFact
-        datosImagen(13,1,1) = datosImagen(13,1,1) + (datos.MajorAxisLength/2);%Radio
-        datosImagen(14,1,1) = datosImagen(14,1,1) + (datos.Area/(pi*(datos.MajorAxisLength/2)^2));%AreaCirc
-        %sacar convexarea, solidity, 
-        %area rect = ejemayor*ejemenor
-        %form_fact = (4.*pi.*area)./(perimetro.^2);
-        %radio = ejemayor/2;
-        %area_circ = area./(pi*radio.^2);
-        
-        %rojo, naranja, yema, amarillo, limon, verde, azul, agua, turqueza, rosa,
-        %morado, rosafosfo, cafe, gris, blanco, negro 
-        
-        
-        %         datosImagen.Area = datosImagen.Area + datos.Area;
-        %         datosImagen.MajorAxisLength = datosImagen.MajorAxisLength + datos.MajorAxisLength;
-        %         datosImagen.MinorAxisLength = datosImagen.MinorAxisLength + datos.MinorAxisLength;
-        %         datosImagen.Eccentricity = datosImagen.Eccentricity + datos.Eccentricity;
-        %         datosImagen.EquivDiameter = datosImagen.EquivDiameter + datos.EquivDiameter;
-        %         datosImagen.Extent = datosImagen.Extent + datos.Extent;
-        %         datosImagen.Perimeter = datosImagen.Perimeter + datos.Perimeter;
-        %         datosImagen.AxisRatio = datosImagen.AxisRatio + (datos.MajorAxisLength/datos.MinorAxisLength);
-        
-        
-%         if(datosImagen(1,1,2) == 0 || datos.Area < datosImagen(1,1,2))
-%            datosImagen(1,1,2)
-%         else
-%             
-%             
-%         end
-        
+    if(j==6 || j==3)figure,imshow(imBin) 
     end
     
     
+    [L, numObj] = bwlabel(imBin);
     
+    %EN CASO DE TERMINAR USANDO ESTRUCTURAS..%
+    %datosImagen = struct('Area',{0},'MajorAxisLength',{0},'MinorAxisLength',{0},'Eccentricity',{0},'EquivDiameter',{0},'Extent',{0},'Perimeter',{0},'AxisRatio',{0});
+    
+    
+    
+    
+    
+    for l=1 :numObj
+        
+        
+        datos = regionprops(L == l, parametros);
+        
+        
+       
+        
+        datosImagen(1,1,j) = datosImagen(1,1,j) + datos.Area;
+        datosImagen(2,1,j) = datosImagen(2,1,j) + datos.MajorAxisLength;
+        datosImagen(3,1,j) = datosImagen(3,1,j) + datos.MinorAxisLength;
+        datosImagen(4,1,j) = datosImagen(4,1,j) + datos.Eccentricity;
+        datosImagen(5,1,j) = datosImagen(5,1,j) + datos.EquivDiameter;
+        datosImagen(6,1,j) = datosImagen(6,1,j) + datos.Extent;
+        datosImagen(7,1,j) = datosImagen(7,1,j) + datos.Perimeter;
+        datosImagen(8,1,j) = datosImagen(8,1,j) + datos.ConvexArea;
+        datosImagen(9,1,j) = datosImagen(9,1,j) + datos.Solidity;
+        %HASTA AQUI SON PARAMETROS OBTENIDOS DIRECTAMENTE%
+        %ESTOS SON PARAMETROS COMBINADOS%
+        datosImagen(10,1,j) = datosImagen(10,1,j) + (datos.MinorAxisLength/datos.MajorAxisLength);%AxisRatio
+        datosImagen(11,1,j) = datosImagen(11,1,j) + (datos.MajorAxisLength*datos.MinorAxisLength);%AreaRect
+        datosImagen(12,1,j) = datosImagen(12,1,j) + ((datos.Area * 4 * pi)/(datos.Perimeter^2));%FormFact
+        datosImagen(13,1,j) = datosImagen(13,1,j) + (datos.MajorAxisLength/2);%Radio
+        datosImagen(14,1,j) = datosImagen(14,1,j) + (datos.Area/(pi*(datos.MajorAxisLength/2)^2));%AreaCirc
+        
+        
+        %rojo, naranja, yema, amarillo, limon, verde, azul, agua, turqueza, rosa,
+        %morado, rosafosfo, cafe, gris, blanco, negro
+        
+        datos = [];
+    end
+    
+    datosImagen(:,:,j) = datosImagen(:,:,j)./numObj;
     
 end
 
 
 
+global datosFinales;
+datosFinales = sum(datosImagen,3);
+datosFinales = datosFinales./6;
+
+
+nombreFinal = strrep(a,' ','s');
+nombreFinal
+whos
+save(nombreFinal,'datosFinales');
 
 
 toc
